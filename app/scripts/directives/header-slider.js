30454d4e126a6de895468e978b415d1546f7dda0
Carlos Jovel <cm.jovel@gmail.com>
2014-09-30 12:06:11 -0600

    images path fix

--- app/scripts/directives/header-slider.js
+++ app/scripts/directives/header-slider.js
@@ -5,11 +5,11 @@ angular.module('torogozApp')
 		return {
 			restrict: 'E',
 			template: 	'<div class="slider">' +
-										'<img src="/images/hero-1.jpg" class="slide">' +
-										'<img src="/images/hero-2.jpg" class="slide">' +
-										'<img src="/images/hero-3.jpg" class="slide">' +
-										'<img src="/images/hero-4.jpg" class="slide">' +
-										'<img src="/images/hero-5.jpg" class="slide">' +
+										'<img src="images/hero-1.jpg" class="slide">' +
+										'<img src="images/hero-2.jpg" class="slide">' +
+										'<img src="images/hero-3.jpg" class="slide">' +
+										'<img src="images/hero-4.jpg" class="slide">' +
+										'<img src="images/hero-5.jpg" class="slide">' +
 									'</div>',
 			link: function() {
 	      $('.slider img:not(:first)').hide();
