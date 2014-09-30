51e35a083698a2ba614ec46886e87855f728a27a
Carlos Jovel <cm.jovel@gmail.com>
2014-09-30 15:26:56 -0600

    center slides

--- app/scripts/directives/header-slider.js
+++ app/scripts/directives/header-slider.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('torogozApp')
-	.directive('headerSlider', function () {
+	.directive('headerSlider', function ($timeout) {
 		return {
 			restrict: 'E',
 			template: 	'<div class="slider">' +
@@ -13,12 +13,28 @@ angular.module('torogozApp')
 									'</div>',
 			link: function() {
 	      $('.slider img:not(:first)').hide();
-	      var fadeNext = function() {
+
+	      setInterval(function() {
 	      	$('.slider img').first().fadeOut().appendTo($('.slider'));
         	$('.slider img').first().fadeIn();
-	      };
+	      }, 8000);
 
-	      setInterval(fadeNext, 8000);
+	      $(window).resize(function(){
+	      	var slidesContainer = $('.slider'),
+	      			slides = slidesContainer.children(),
+	      			image = $(slides[0]),
+	      			parent = slidesContainer.parent();
+
+	      	if (parent.outerWidth() < image.outerWidth()) { // Image is larger than viewport
+	      		var excedent = image.width() - parent.width();
+
+	      		slides.each(function(){
+	      			$(this).css({'left': excedent/-2});
+	      		});
+	      	}
+	      });
+
+	      $timeout(function(){ $(window).resize(); }, 500);
 	    },
 			replace: true
 		};
